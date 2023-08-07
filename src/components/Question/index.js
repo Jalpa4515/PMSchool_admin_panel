934b85c9db0feb12133fdef6687c1d1cae55af51
Saad Karim <saadkarim94356@gmail.com>
2023-08-07 23:33:49 +0600

    fixed

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import {
+ DropdownItem,
+ DropdownMenu,
+ DropdownToggle,
+ UncontrolledDropdown,
+} from "reactstrap";
 import ArrowDownIcon from "../../assets/img/icons/common/down-arrow.svg";
 import EditIcon from "../../assets/img/icons/common/pen-to-square-solid.svg";
 
 const Question = () => {
  const [isOpen, setIsOpen] = useState(false);
+ const [category, setCategory] = useState("");
  return (
   <div className="question border-bottom border-light py-3">
    <div className="question__head d-flex align-items-center">
@@ -17,25 +24,49 @@ const Question = () => {
     </span>
     <span className="ml-3 font-weight-700 text-dark">Question</span>
     <button
-     className="edit__btn ml-auto border-0 p-0 d-flex justify-content-center align-items-center mr-5"
+     className="edit__btn ml-auto border-0 p-0 d-flex justify-content-center align-items-center mr-5 bg-transparent"
      style={{
       width: "14px",
       height: "14px",
      }}>
-     <img src={EditIcon} alt="" />
+     <img src={EditIcon} alt="" className="w-100 h-100" />
     </button>
     <button
-     className="extend__btn border-0 p-0 d-flex justify-content-center align-items-center"
+     className="extend__btn border-0 p-0 d-flex justify-content-center align-items-center bg-transparent"
      style={{
       width: "20px",
       height: "20px",
      }}
      onClick={() => setIsOpen(!isOpen)}>
-     <img src={ArrowDownIcon} alt="" />
+     <img src={ArrowDownIcon} alt="" className="w-full h-full" />
     </button>
    </div>
    <div className={`question__body ${isOpen ? "d-block" : "d-none"}`}>
     <form action="" className="mt-4 ml-5 pr-5 d-flex flex-column">
+     <UncontrolledDropdown nav className="mb-3">
+      <DropdownToggle className="nav-link-icon">
+       {category || "Select Category"}
+      </DropdownToggle>
+      <DropdownMenu
+       aria-labelledby="navbar-default_dropdown_1"
+       className="dropdown-menu-arrow"
+       left>
+       <DropdownItem onClick={(e) => setCategory(e.target.innerText)}>
+        User Research
+       </DropdownItem>
+       <DropdownItem onClick={(e) => setCategory(e.target.innerText)}>
+        User Research
+       </DropdownItem>
+       <DropdownItem divider />
+       <DropdownItem onClick={(e) => setCategory(e.target.innerText)}>
+        User Research
+       </DropdownItem>
+       <DropdownItem divider />
+       <DropdownItem onClick={(e) => setCategory(e.target.innerText)}>
+        User Research
+       </DropdownItem>
+      </DropdownMenu>
+     </UncontrolledDropdown>
      <div className="question__text d-flex flex-column">
       <label htmlFor="addQuestion">Add question</label>
       <textarea
